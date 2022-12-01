6e8026b8f9346b8166f7621394541cd31e0c5820
Mirko <mirkopoj@hotmail.com>
2022-12-01 18:37:17 -0300

    punto3

--- simulacion.jl
+++ simulacion.jl
@@ -1,30 +1,55 @@
 using PlotlyJS
 using StatsBase
 using DSP
+using Distributions
+using FFTW
 
-L = 100
+L = 128
+N = 500
+d = Normal()
 
-S1 = rand(-1:2:1, 1, L)
-S2 = rand(-1:2:1, 1, L)
+#X1 = rand(d, L)
+#X2 = rand(d, L)
+#X3 = rand(d, L)
+#X4 = rand(d, L)
+#
+#P1 = fft(X1)
+#P2 = fft(X2)
+#P3 = fft(X3)
+#P4 = fft(X4)
+#
+#S1 = (abs.(P1).^2)/L
+#S2 = (abs.(P2).^2)/L
+#S3 = (abs.(P3).^2)/L
+#S4 = (abs.(P4).^2)/L
 
-d1 = [1,1,-1,-1,1,1,-1,-1]
-d2 = [-1,1,-1,1,1,-1,1,-1]
+t = 0:L
+freqs = fftfreq(length(t), 1)
+freqs = fftshift(freqs)
 
-M1 = reshape(transpose(d1*S1),:,1)
-M2 = reshape(transpose(d2*S2),:,1)
+S = ones(Float32,L)
 
-p1 = plot(scatter(mode="lines", x=0:length(d1), y=d1, name="d1", line_shape="hvh",line=attr(color="royalblue")))
-p2 = plot(scatter(mode="lines", x=0:length(d2), y=d2, name="d2", line_shape="hvh",line=attr(color="firebrick")))
-p3 = plot(scatter(mode="lines", x=0:length(S1), y=S1[:], name="S1", line_shape="hvh",line=attr(color="royalblue")))
-p4 = plot(scatter(mode="lines", x=0:length(S2), y=S2[:], name="S2", line_shape="hvh",line=attr(color="firebrick")))
-p5 = plot(scatter(mode="lines", x=1:length(M1), y=M1[:], name="M1", line_shape="hvh",line=attr(color="royalblue")))
-p6 = plot(scatter(mode="lines", x=1:length(M2), y=M2[:], name="M2", line_shape="hvh",line=attr(color="firebrick")))
+#t1 = scatter(mode="lines", x=t, y=abs.(S1), name="S1")
+#t2 = scatter(mode="lines", x=t, y=abs.(S2), name="S2")
+#t3 = scatter(mode="lines", x=t, y=abs.(S3), name="S3")
+#t4 = scatter(mode="lines", x=t, y=abs.(S4), name="S4")
+t5 = scatter(mode="lines", x=t, y=S, name="Analitico")
+#
+#display( plot([t1, t2, t3, t4, t5]) )
 
-p =[p1 p2; p3 p4; p5 p6]
-relayout!(p)
-display(p)
+S = zeros(Float32,L)
+for i in 1:N
+	X = rand(d,L)
+	P = fft(X)
+	global S += (abs.(P).^2)/L
+end
+S /= N
 
-n = 5
+t1 = scatter(mode="lines", x=t, y=abs.(S), name="S")
+display( plot([t1, t5]) )
+
+readline()
+
 #N = rand(-n:0.1:n,1,L*8)
 #N = transpose(N)
 R = range(0, L, length=L*8)
