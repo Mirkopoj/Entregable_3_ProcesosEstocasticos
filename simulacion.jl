1e784e6d594634753b4e3d48e45a50c7498285d5
Mirkopoj <mirkopoj@hotmail.com>
2022-12-01 20:57:00 -0300

    Listo

--- simulacion.jl
+++ simulacion.jl
@@ -5,7 +5,7 @@ using Distributions
 using FFTW
 
 L = 128
-N = 500
+N = 2
 d = Normal()
 
 #X1 = rand(d, L)
@@ -38,44 +38,19 @@ t5 = scatter(mode="lines", x=t, y=S, name="Analitico")
 #display( plot([t1, t2, t3, t4, t5]) )
 
 S = zeros(Float32,L)
+V = S
 for i in 1:N
 	X = rand(d,L)
 	P = fft(X)
-	global S += (abs.(P).^2)/L
+	Per = (abs.(P).^2)/L
+	global S += Per
+	global V += (Per.-1).^2
 end
 S /= N
+V /= (N-1)
 
-t1 = scatter(mode="lines", x=t, y=abs.(S), name="S")
-display( plot([t1, t5]) )
-
-readline()
-
-#N = rand(-n:0.1:n,1,L*8)
-#N = transpose(N)
-R = range(0, L, length=L*8)
-N = @. sin(R)* n + 0.01 * randn() 
-M = vec(M1 .+ M2 .+ N)
-
-display(plot(scatter(mode="lines", x=1:length(M), y=M[:], name="M", line_shape="hvh",line=attr(color="green"))))
-display(plot(scatter(mode="lines", x=1:length(N), y=N[:], name="N", line_shape="hvh",line=attr(color="green"))))
-
-l=vec(-L:L*8-1)
-S1 = vec(hcat(S1,zeros(Int64,1,L*7)))
-R1 = crosscor(S1,M,l;demean=false)*(L*8+L*8-1)
-
-S2 = vec(hcat(S2,zeros(Int64,1,L*7)))
-R2 = crosscor(S2,M,l;demean=false)*(L*8+L*8-1)
-
-responsetype = Bandstop(0.015, 0.03; fs=1)
-designmethod = Butterworth(6)
-R1 = filt(digitalfilter(responsetype, designmethod), R1)
-R2 = filt(digitalfilter(responsetype, designmethod), R2)
-
-p7 = plot(scatter(mode="lines", x=1:length(R1), y=R1[:], name="R1", line_shape="hvh",line=attr(color="royalblue")))
-p8 = plot(scatter(mode="lines", x=1:length(R2), y=R2[:], name="R2", line_shape="hvh",line=attr(color="firebrick")))
-
-r =[p7 p8]
-relayout!(r)
-display(r)
+t1 = scatter(mode="lines", x=t, y=abs.(S), name="Sx")
+t2 = scatter(mode="lines", x=t, y=abs.(V), name="Var")
+display( plot([t1, t2, t5]) )
 
 readline()
